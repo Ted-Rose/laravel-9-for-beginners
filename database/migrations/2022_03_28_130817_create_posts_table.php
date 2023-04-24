c390d741e3ef10a13a8a9dcc8b06c9e933cf0d9b
Tedis Rozenfelds <trozenfelds@gocardless.com>
2023-04-24 17:28:19 +0300

    Added comments

--- database/migrations/2022_03_28_130817_create_posts_table.php
+++ database/migrations/2022_03_28_130817_create_posts_table.php
@@ -14,13 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
+            // Creates table posts
             $table->id();
+            // id is by default primary, but we can add primary method
+            // first method makes column first in table
             $table->string('title')->unique();
+            // Unique method defines that this will be unique
             $table->text('excerpt')->nullable();
+            // Allows null values to be inserted into columns
             $table->text('body');
             $table->text('image_path');
+            // Path to an image
             $table->boolean('is_published');
             $table->integer('min_to_read')->default(1);
+            // This default method sets default value
+            // Show user how long time it takes to read a post
             $table->timestamps();
         });
     }
